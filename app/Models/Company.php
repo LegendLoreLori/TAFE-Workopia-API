2e512b2da702cae803c20963c4e0d867b444f1fe
Lori <lorenzomandolfino@gmail.com>
2024-08-29 14:35:08 +0800

    feat: Add Company model and migration

---
+++ app/Models/Company.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, mixed>
+     */
+    protected $fillable = [
+        'name',
+        'city',
+        'state',
+        'country',
+        'image_path'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+        ];
+    }
+}
