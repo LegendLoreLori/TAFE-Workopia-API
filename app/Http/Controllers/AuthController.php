fc1eb6a077ff26fb35d90ea8aadece1c263e46c0
Lori <lorenzomandolfino@gmail.com>
2024-10-22 12:43:44 +0800

    feat: Auth
    - add `AuthController.php`
    - add sanctum auth middleware to api route groups
    - remove unused user route in `api.php`

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    /**
+     * Create a sanctum token for an existing user
+     *
+     * @param  Request  $request
+     * @return JsonResponse
+     */
+    public function login(Request $request): JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            $messages = $validator->errors()->messages();
+            return self::sendFailure($messages, 422);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Login invalid'
+            ], 503);
+        }
+
+        return response()->json(["token" => $user->createToken($request->device_name)->plainTextToken]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,18 @@
 <?php
 
 use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PositionController;
 use App\Http\Controllers\RegionController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/user', function (Request $request) {
-//    return $request->user();
-//})->middleware('auth:sanctum');
-
-Route::group(['prefix' => 'v1'], function () {
+Route::group(['prefix' => 'v1', 'middleware' => ['auth:sanctum']], function () {
     Route::get('regions', [RegionController::class, 'index']);
     Route::get('regions/{id}', [RegionController::class, 'show']);
-});
+})->middleware('auth:sanctum');
 
-Route::group(['prefix' => 'v1'], function () {
+Route::group(['prefix' => 'v1', 'middleware' => ['auth:sanctum']], function () {
     Route::get('/users/trash/{id}', [UserController::class, 'restore']);
     Route::get('/companies/trash/{id}', [CompanyController::class, 'restore']);
     Route::get('/positions/trash/{id}', [PositionController::class, 'restore']);
@@ -28,3 +24,5 @@ Route::group(['prefix' => 'v1'], function () {
         ]
     );
 });
+
+Route::post('/login', [AuthController::class, 'login']);
